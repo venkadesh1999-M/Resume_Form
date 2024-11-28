5600573b767ff2d38cc4b7c89a8ddbe5423be09f
venkadesh1999-M <messivenkadesh135@gmail.com>
2024-11-28 20:13:32 +0530

    Resume Form completed

---
+++ main.js
@@ -0,0 +1,90 @@
+let resume = 
+    {
+    name:"venkadesh",
+    age:25,
+    phone_number:"8838946021",
+    email:"venkadesh@gmail.com",
+    address:"22D4,Aasari Maar Pillayar koil Street,Vadesery,Nagercoil",
+    objective:"I am seeking a competitive and challenging environment where I can serve your organization and establish a career for myself.",
+    skills:
+      [
+        "Excel",
+        "Fullstack_developer",
+      ],
+      
+    language_known:
+      [
+         "tamil",
+         "English",
+      ],
+      
+    education:
+      [
+        {
+          course_name:"SSLC",
+          institude_name:"S.M.R.V",
+          year:2014,
+          place:"Nagercoil",
+        },
+        {
+          course_name:"HSC",
+          institude_name:"S.M.R.V",
+          year:2016,
+          place:"Nagercoil",
+        },
+         {
+          course_name:"Bsc.Computer_Science",
+          institude_name:"S.T.Hindu College",
+          year:2019,
+          place:"Nagercoil",
+        },
+      ],
+      
+      certification:
+      [
+        {
+          course_name:"Full_Stack Developer",
+          acadamey:"Agaram Software Acadamey",
+          contact_number:"094440 74214",
+          year:"2024-2025",
+          duration:"6 months",
+        },
+    ],
+
+      projects:
+      [
+        {
+          project:"Web-Development",
+          name:"social_media App",
+        },
+     ],
+      
+      work_experience:
+      [
+        {
+            year: 2,
+            company_name:"Vision-Group_Retail.technologies",
+            designation:"Process Associate",
+            place:"Nagercoil",
+         },
+     ],
+
+      personal_details:
+      {
+        name:"Venkadesh",
+        dob:"11-05-1999",
+        father_name:"R.Muthu_Krishnan",
+        mother_name:"N.Sheela",
+        mobile_number:"8754640796",
+        hobbies:
+        [
+            "Photoshop",
+            "Cricket",
+            "Football",
+            "movies",
+            "chess",
+        ],
+      }
+ }
+// console.log(resume.education[0])
+console.log(resume.certification.acadamey)
